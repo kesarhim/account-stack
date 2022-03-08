e4981d727d3006c9658edef85d90d49d206c5982
rajat26oct <62444798+rajat26oct@users.noreply.github.com>
2022-03-08 16:14:24 +0530

    payment done

--- src/app/components/payment/customer-payment-history/customer-payment-history-component.ts
+++ src/app/components/payment/customer-payment-history/customer-payment-history-component.ts
@@ -1,50 +1,87 @@
-import { Component, OnInit } from '@angular/core';
-
-import {ViewChild} from '@angular/core';
-import {MatTable} from '@angular/material/table';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+import { Location } from '@angular/common';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { MatTable } from '@angular/material/table';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertService } from '../../core/components/alert/alert.service';
+import { LoaderService } from '../../core/components/loader/loader.service';
+import { Customer } from '../../customer/create/models/customer-model';
+import { CustomerService } from '../../customer/service/customer-service';
+import { DrawerService } from '../../shared/drawer/drawer.service';
+import { PaymentDetails } from '../models/payment.model';
 
 @Component({
   selector: 'app-customer-payment-history',
   templateUrl: './customer-payment-history-component.html',
-  styleUrls:['./customer-payment-history-component.css']
+  styleUrls: ['./customer-payment-history-component.css'],
 })
-
 export class CustomerPaymentHistoryComponent implements OnInit {
-  constructor() { }
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = [...ELEMENT_DATA];
+  @ViewChild('receivePaymentTemp') receivePaymentTemplate: TemplateRef<any>;
 
-  @ViewChild(MatTable) table: MatTable<PeriodicElement>;
+  constructor(
+    private route: ActivatedRoute,
+    private customerService: CustomerService,
+    private loaderService: LoaderService,
+    private alertService: AlertService,
+    private _location: Location,
+    private router: Router,
+    private drawerService: DrawerService
+  ) {}
 
-  addData() {
-    const randomElementIndex = Math.floor(Math.random() * ELEMENT_DATA.length);
-    this.dataSource.push(ELEMENT_DATA[randomElementIndex]);
-    this.table.renderRows();
+  public selectedCustomer: Customer | null;
+  public customerId: number;
+  public paymentDetail: PaymentDetails | null;
+
+  ngOnInit() {}
+
+  onSelectCustomer = (customer: Customer) => {
+    this.selectedCustomer = null;
+    this.loaderService.show();
+    this.customerService
+      .getCustomerDetailsById(customer.id)
+      .subscribe((result: any) => {
+        this.loaderService.hide();
+        if (result) {
+          this.selectedCustomer = result.response;
+        }
+      },err=>this.loaderService.hide());
+  };
+
+  backClicked() {
+    this._location.back();
   }
 
-  removeData() {
-    this.dataSource.pop();
-    this.table.renderRows();
-  }
-  ngOnInit() { }
+  getPaymentDetails = (customerDetail: Customer): PaymentDetails | null => {
+    if (customerDetail) {
+      let paymentDetails: PaymentDetails = new PaymentDetails();
+      paymentDetails.customerId = customerDetail.id;
+      paymentDetails.totalFees = customerDetail.totalFees;
+      paymentDetails.feesPaid = customerDetail.feesPaid;
+      paymentDetails.balanceAmount = customerDetail.balanceAmount;
+      paymentDetails.clientName = customerDetail.fullName;
+      paymentDetails.panNo = customerDetail.panNo;
+      paymentDetails.discountOffered = customerDetail.discountOffered;
+      paymentDetails.contextKey = 'CUSTOMER';
+      return paymentDetails;
+    }
+    return null;
+  };
+
+  onReceivePayment = (value: Customer) => {
+    this.selectedCustomer = value;
+    this.paymentDetail = this.getPaymentDetails(value);
+    this.drawerService.openDrawer(
+      this.receivePaymentTemplate,
+      'Receive Payment',
+      'payments'
+    );
+  };
+
+  onEditCandidate = (customer: Customer) => {
+    if (customer) {
+      this.router.navigate(['/home/customer/create'], {
+        queryParams: { customerId: customer.id },
+      });
+    }
+  };
 }
